d718154770bae3b212e98511842d0e68964a8f2f
Biraj Singha <67847698+BirajSingha@users.noreply.github.com>
2024-07-24 20:44:45 +0530

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { MailModule } from './mail/mail.module';
+import { FriendsModule } from './friends/friends.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { MailModule } from './mail/mail.module';
     AuthModule,
     UsersModule,
     MailModule,
+    FriendsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
